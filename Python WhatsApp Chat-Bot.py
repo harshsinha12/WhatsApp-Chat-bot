207a1a29bd5697a65d14da7d5389f21ae69fa7a9
Harsh Sinha <78256003+harshsinha12@users.noreply.github.com>
2021-04-30 16:25:05 +0530

    Update Python WhatsApp Chat-Bot.py

--- Python WhatsApp Chat-Bot.py
+++ Python WhatsApp Chat-Bot.py
@@ -4,7 +4,7 @@ import time
 # To Make sure the program runs perfectly you need to:
 # i) Have logged in to WhatsApp earlier on the same device through Chrome
 # ii) Chrome is open in Background
-# iii) As WhatsApp loads click on the profile and wait
+# iii) As WhatsApp loads click on the profile you wish to send the message and wait 20 seconds
 # iv) Make sure Caps Lock isn't ON
 n = int(input("Enter the number of times you wish to print:"))
 text = input("Enter the text you wish to send:")
